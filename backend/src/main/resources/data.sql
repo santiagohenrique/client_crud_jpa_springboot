35c2dff227e26cb2d471399edb264c27faff2b8a
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-01-15 19:25:51 -0300

    Fixing data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro', '393939', 3000.0, NOW(), 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro', '393939', 3000.0, '1990-05-15', 3);
