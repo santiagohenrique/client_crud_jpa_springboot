3d2fdf873c8c51c04212973032402d5630f2be50
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-01-15 19:44:46 -0300

    Inserting more data into the database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro', '393939', 3000.0, '1990-05-15', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria', '484848', 5000.0, '1985-08-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos', '575757', 4000.0, '1992-03-10', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana', '631213', 6000.0, '1980-12-05', 0);
